0facbcc865f2812640053c650b7f186f99bef265
Alex <alex.thiel@aotu.ai>
2020-05-08 21:48:07 -0700

    Moved world => controller.py

---
+++ crawlai/controller.py
@@ -0,0 +1,14 @@
+from godot import exposed, export
+from godot.bindings import Node2D, ResourceLoader
+
+
+@exposed
+class World(Node2D):
+	min_num_critters = export(int, 1000)
+
+	def _ready(self):
+		critter = ResourceLoader.load("res://Game/Critter/Critter.tscn")
+		for i in range(0, self.min_num_critters):
+			instance = critter.instance()
+			self.add_child(instance)
+		print(f"Created {self.min_num_critters} critters")

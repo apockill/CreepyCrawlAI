300036fec15ea32428ef5a52aa88ed28ca47dbf9
Alex <alex.thiel@aotu.ai>
2020-05-13 22:43:23 -0700

    Reorganized codebase to be item-oriented. Added a food griditem, and adjusted assumptions that had broken in the tests.

--- crawlai/grid.py
+++ crawlai/grid.py
@@ -32,7 +32,40 @@ class Grid:
 		for grid_item in self.id_to_obj.values():
 			yield grid_item
 
-	def add_item(self, pos: Position, grid_item: GridItem):
+	def get_grid_around(self, pos: Position, radius: int) -> np.ndarray:
+		"""Get a numpy array of obj IDs surrounding a particular area.
+		This function will always return an array of shape (radius, radius),
+		where the value is the object ID. """
+
+		x1, y1 = pos.x - radius, pos.y - radius
+		x2, y2 = pos.x + radius + 1, pos.y + radius + 1
+
+		crop = self._grid[
+			   np.clip(x1, 0, self.width):np.clip(x2, 0, self.width),
+			   np.clip(y1, 0, self.height):np.clip(y2, 0, self.height)]
+		w, h = crop.shape
+		if x1 < 0:
+			concat = np.full((abs(x1), h),
+							 fill_value=-1, dtype=np.int8)
+			crop = np.vstack((concat, crop))
+		if x2 > self.width:
+			concat = np.full((x2 - self.width, h),
+							 fill_value=-1, dtype=np.int8)
+			crop = np.vstack((crop, concat))
+
+		w, h = crop.shape
+		if y1 < 0:
+			concat = np.full((w, abs(y1)),
+							 fill_value=-1, dtype=np.int8)
+			crop = np.hstack((concat, crop))
+		if y2 > self.height:
+			concat = np.full((w, y2 - self.height),
+							 fill_value=-1, dtype=np.int8)
+			crop = np.hstack((crop, concat))
+		return crop
+
+	def add_item(self, pos: Position, grid_item: GridItem) \
+			-> bool:
 		if not self.is_empty_coord(pos):
 			grid_item.instance.queue_free()
 			return False
@@ -46,12 +79,14 @@ class Grid:
 		assert self.try_move_item(pos, grid_item, is_new=True)
 		return True
 
-	def move_item_relative(self, rel_pos: Position, grid_item: GridItem):
-		self.try_move_item(
+	def move_item_relative(self, rel_pos: Position, grid_item: GridItem) \
+			-> bool:
+		return self.try_move_item(
 			pos=self.id_to_pos[grid_item.id] + rel_pos,
 			grid_item=grid_item)
 
-	def try_move_item(self, pos: Position, grid_item: GridItem, is_new=False):
+	def try_move_item(self, pos: Position, grid_item: GridItem, is_new=False) \
+			-> bool:
 		if not self.is_empty_coord(pos):
 			return False
 		if not is_new:
--- crawlai/items/critter/mixins/ai_mixin.py
+++ crawlai/items/critter/mixins/ai_mixin.py
@@ -1,5 +1,7 @@
 import random
 
+import numpy as np
+
 from crawlai.items.critter.base_critter import BaseCritter
 from crawlai.position import Position
 
@@ -27,10 +29,19 @@ class AICritterMixin(BaseCritter):
 		- Train each step on the outcome of the scenario after running
 	"""
 	CHOICES = [Position(*c) for c in [(0, 0), (0, 1), (1, 0), (-1, 0), (0, -1)]]
+	AREA_AROUND = 10
 
 	def __init__(self):
 		super().__init__()
 
-	def get_move(self, inputs) -> Position:
+	def get_move(self, grid) -> Position:
 		"""Super smart AI goes here"""
+
+		"""
+		Indexing: 
+		input[x][y][Z] where Z can be 0: Critter, 1: Food
+		"""
+		net_input = grid.get_grid_around(
+			pos=grid.id_to_pos[self.id],
+			radius=self.AREA_AROUND)
 		return random.choice(self.CHOICES)

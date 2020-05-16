6ab115d9e0b2c2d4f3f6e02cff9726a640a043ca
Alex <alex.thiel@aotu.ai>
2020-05-16 14:59:44 -0700

    Moved rendering logic entirely from Grid to World, and added tests to ensure consistency of GameObjects within grid, world, etc.

--- crawlai/game_scripts/world.py
+++ crawlai/game_scripts/world.py
@@ -1,50 +1,77 @@
+from typing import Type, Optional
+
 from godot import exposed, export
-from godot.bindings import Node2D
+from godot.bindings import Node2D, Vector2
 
 from crawlai.grid import Grid
+from crawlai.grid_item import GridItem
 from crawlai.items.critter.base_critter import BaseCritter
 from crawlai.items.critter.critter import Critter
 from crawlai.items.food import Food
+from crawlai.position import Position
 
 
 @exposed
 class World(Node2D):
-	min_num_critters = export(int, 100)
-	min_num_food = export(int, 10)
+	min_num_critters = export(int, 0)
+	min_num_food = export(int, 0)
 	grid_width = export(int, 100)
 	grid_height = export(int, 100)
-	grid_spacing = export(int, 100)
+	spacing = export(int, 100)
+	rendering: bool = True
 
 	def _ready(self):
 		self.grid = Grid(
 			width=self.grid_width,
-			height=self.grid_height,
-			spacing=self.grid_spacing,
-			root_node=self)
+			height=self.grid_height)
 		for i in range(0, self.min_num_critters):
-			self.grid.add_item(self.grid.random_free_cell, Critter())
+			self.add_item(self.grid.random_free_cell, Critter)
 		for i in range(0, self.min_num_food):
-			self.grid.add_item(self.grid.random_free_cell, Food())
+			self.add_item(self.grid.random_free_cell, Food)
 		print("Created", self.min_num_critters, "Critters")
 		print("Created", self.min_num_food, "Foods")
 
 	def _process(self, delta):
-		# Run tick for all grid items
-		for grid_item in self.grid:
-			grid_item.tick()
+		self.step(self.grid)
 
-		moves = {}
-		# Process critter moves here, in the future in another thread
-		for grid_item in self.grid:
-			if isinstance(grid_item, BaseCritter):
-				moves[grid_item.id] = grid_item.get_move(self.grid)
-
-		# Actually move the critters here
-		for grid_item in self.grid:
-			if isinstance(grid_item, BaseCritter):
-				self.grid.move_item_relative(moves[grid_item.id], grid_item)
+		# Render all sprites
+		if self.rendering:
+			for grid_item in self.grid:
+				pos = self.grid.id_to_pos[grid_item.id]
+				grid_item.instance.set_position(
+					Vector2(pos.x * self.spacing, pos.y * self.spacing))
 
 	def _on_render_button_toggled(self, button_pressed):
 		""" Enable and disable rendering
 		Connected to: GUI.RenderButton """
-		self.grid.rendering = button_pressed
+		self.rendering = button_pressed
+
+	def add_item(self, pos: Position,
+				 item_type: Type[GridItem]) -> Optional[GridItem]:
+		item = item_type()
+		successful = self.grid.add_item(pos=pos, grid_item=item)
+		if successful:
+			self.add_child(item.instance)
+			return item
+		else:
+			item.instance.queue_free()
+			return None
+
+	@staticmethod
+	def step(grid: Grid):
+		"""Perform the logic for one full grid step"""
+
+		# Run tick for all grid items
+		for grid_item in grid:
+			grid_item.tick()
+
+		moves = {}
+		# Process critter moves here, in the future in another thread
+		for grid_item in grid:
+			if isinstance(grid_item, BaseCritter):
+				moves[grid_item.id] = grid_item.get_move(grid)
+
+		# Actually move the critters here
+		for grid_item in grid:
+			if isinstance(grid_item, BaseCritter):
+				grid.move_item_relative(moves[grid_item.id], grid_item)
--- crawlai/grid.py
+++ crawlai/grid.py
@@ -10,9 +10,8 @@ from crawlai.math_utils import clamp
 
 
 class Grid:
-	def __init__(self, width, height, spacing, root_node):
+	def __init__(self, width, height):
 		# Grid parameters
-		self._spacing: int = spacing
 		self.width: int = width
 		self.height: int = height
 
@@ -22,39 +21,32 @@ class Grid:
 		self.id_to_obj: Dict[int, GridItem] = {}
 		self.id_to_pos: Dict[int, Position] = {}
 
-		# World node
-		self._root_node: Node = root_node
-
-		# Settings
-		self.rendering = True
-		"""When false, object instances don't get set_position called"""
-
 	def __iter__(self):
 		for grid_item in self.id_to_obj.values():
 			yield grid_item
 
 	def add_item(self, pos: Position, grid_item: GridItem) -> bool:
 		if not self.is_empty_coord(pos):
-			grid_item.instance.queue_free()
 			return False
 
 		assert self.array[pos.x][pos.y] == 0
+
 		# Register the item
 		self.id_to_obj[grid_item.id] = grid_item
-		self._root_node.add_child(grid_item.instance)
 
 		# Move the item to the correct place
 		assert self.try_move_item(pos, grid_item, is_new=True)
 		return True
 
-	def move_item_relative(self, rel_pos: Position, grid_item: GridItem) \
-			-> bool:
+	def move_item_relative(self, rel_pos: Position,
+						   grid_item: GridItem) -> bool:
 		return self.try_move_item(
 			pos=self.id_to_pos[grid_item.id] + rel_pos,
 			grid_item=grid_item)
 
-	def try_move_item(self, pos: Position, grid_item: GridItem, is_new=False) \
-			-> bool:
+	def try_move_item(self, pos: Position,
+					  grid_item: GridItem,
+					  is_new=False) -> bool:
 		if not self.is_empty_coord(pos):
 			return False
 		if not is_new:
@@ -66,9 +58,6 @@ class Grid:
 		self.array[pos.x][pos.y] = grid_item.id
 		self.id_to_pos[grid_item.id] = pos
 
-		if self.rendering:
-			grid_item.instance.set_position(
-				Vector2(pos.x * self._spacing, pos.y * self._spacing))
 		return True
 
 	def is_empty_coord(self, pos: Position):

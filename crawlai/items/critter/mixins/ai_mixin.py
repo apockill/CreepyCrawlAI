df881724c34dc5672e445eff6e08cc1e6060b846
Alex <alex.thiel@aotu.ai>
2020-05-21 21:04:45 -0700

    Added the TF Agents Environment code and initialization code. By adding a threadpool, I am able to more closely control threading for AICritterMixin

--- crawlai/items/critter/mixins/ai_mixin.py
+++ crawlai/items/critter/mixins/ai_mixin.py
@@ -1,41 +1,67 @@
 import random
+from multiprocessing.pool import ThreadPool
+
+import numpy as np
+import tensorflow as tf
+from tf_agents.agents import DqnAgent
+from tf_agents.networks.q_network import QNetwork
+from tf_agents.utils import common
+from tf_agents.environments.tf_py_environment import TFPyEnvironment
 
 from crawlai.items.critter.base_critter import BaseCritter
 from crawlai.position import Position
 from crawlai.grid import Grid
 from crawlai.turn import Turn
+from crawlai.model.environment import CritterEnvironment
 from crawlai.model.extract_inputs import get_instance_grid
+
+threadpool = None
+"""This is a shared threadpool by all AICritterMixins"""
 
 
 class AICritterMixin(BaseCritter):
-	"""
-	Terms
-		V: Velocity (x, y components)
-
-	Inputs:
-		self.vx
-		self.vy
-		self.health
-
-		Nearest 10 critters:
-			vx
-			vy
-			health
-	Outputs:
-		self.vx
-		self.vy
-
-	Desired Features:
-		- Add a self.signal
-		- Train each step on the outcome of the scenario after running
-	"""
 	CHOICES = [Turn(Position(*c), is_action)
 			   for c in [(0, 1), (1, 0), (-1, 0), (0, -1)]
 			   for is_action in (True, False)] + [Turn(Position(0, 0), False)]
-	AREA_AROUND = 20
 
-	def __init__(self):
+	def __init__(self, environment: CritterEnvironment = None):
 		super().__init__()
+		global threadpool
+		threadpool = threadpool or ThreadPool(processes=1)
+		self.environment = (environment or
+							TFPyEnvironment(CritterEnvironment(),
+											isolation=threadpool))
+		assert len(self.CHOICES) == self.environment.action_spec()[0].maximum
+
+		# Hyperparameters
+		num_iterations = 20000
+
+		initial_collect_steps = 1000
+		collect_steps_per_iteration = 1
+		replay_buffer_max_length = 100000
+
+		batch_size = 64
+		learning_rate = 1e-3
+		log_interval = 200
+
+		num_eval_episodes = 10
+		eval_interval = 1000
+
+		# The number and size of the model's hidden layers
+		fc_layer_params = (100,)
+		train_step_counter = tf.Variable(0)
+		q_net = QNetwork(
+			self.environment.observation_spec(),
+			self.environment.action_spec(),
+			fc_layer_params=fc_layer_params)
+		self.agent = DqnAgent(
+			self.environment.time_step_spec(),
+			self.environment.action_spec(),
+			q_network=q_net,
+			optimizer=tf.compat.v1.train.AdamOptimizer(
+				learning_rate=learning_rate),
+			td_errors_loss_fn=common.element_wise_squared_loss,
+			train_step_counter=train_step_counter)
 
 	def get_turn(self, grid: Grid) -> Turn:
 		"""Super smart AI goes here"""
---
+++ crawlai/model/environment.py
@@ -0,0 +1,59 @@
+import numpy as np
+from tf_agents.environments.py_environment import PyEnvironment
+from tf_agents.specs import array_spec
+
+from crawlai.model.extract_inputs import INPUT_SHAPE, INPUT_DTYPE
+
+
+class CritterEnvironment(PyEnvironment):
+
+	def __init__(self):
+		super().__init__()
+		self._observation_spec = array_spec.BoundedArraySpec(
+			shape=INPUT_SHAPE, dtype=INPUT_DTYPE,
+			minimum=0, maximum=3,
+			name="observation")
+
+		# TODO: Figure out how to get len(AiCritterMixin.CHOICES)
+		self._action_spec = [array_spec.BoundedArraySpec(
+			shape=(), dtype=np.int32,
+			minimum=0, maximum=9,
+			name=f"action")]
+
+	def _step(self, action):
+		"""Updates the environment according to action and returns a `TimeStep`.
+
+		See `step(self, action)` docstring for more details.
+
+		Args:
+		  action: A NumPy array, or a nested dict, list or tuple of arrays
+			corresponding to `action_spec()`.
+		"""
+
+	def _reset(self):
+		"""Starts a new sequence, returns the first `TimeStep` of this sequence.
+
+		See `reset(self)` docstring for more details
+		"""
+
+	def observation_spec(self):
+		"""Defines the observations provided by the environment.
+
+		May use a subclass of `ArraySpec` that specifies additional properties such
+		as min and max bounds on the values.
+
+		Returns:
+		  An `ArraySpec`, or a nested dict, list or tuple of `ArraySpec`s.
+		"""
+		return self._observation_spec
+
+	def action_spec(self):
+		"""Defines the actions that should be provided to `step()`.
+
+		May use a subclass of `ArraySpec` that specifies additional properties such
+		as min and max bounds on the values.
+
+		Returns:
+		  An `ArraySpec`, or a nested dict, list or tuple of `ArraySpec`s.
+		"""
+		return self._action_spec

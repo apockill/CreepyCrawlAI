5ed6ce5c067150039df588998ce0c699b9fc308f
Alex <alex.thiel@aotu.ai>
2020-05-31 21:13:55 -0700

    Added a Timer context manager

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,2 +1,3 @@
 from .grid import validate_grid
 from .thread_validation import verify_all_threads_closed
+from .timing import Timer
---
+++ tests/helpers/timing.py
@@ -0,0 +1,19 @@
+from time import time
+
+
+class Timer:
+	def __init__(self):
+		self.start, self.end = None, None
+
+	def __enter__(self):
+		self.start = time()
+		return self
+
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		self.end = time()
+
+	@property
+	def elapsed(self):
+		if self.end is None:
+			return time() - self.start
+		return self.end - self.start

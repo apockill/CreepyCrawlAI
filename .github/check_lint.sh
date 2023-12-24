f2fc463328057ae3f66e60f5d086c7e8192fd7bd
Alex Thiele <alex@urbanmachine.build>
2023-12-23 21:06:16 -0800

    Lint passes

--- .github/check_lint.sh
+++ .github/check_lint.sh
@@ -2,10 +2,10 @@
 set -euxo pipefail
 
 poetry run cruft check
-poetry run mypy --ignore-missing-imports creepycrawlai/ tests/
-poetry run isort --check --diff creepycrawlai/ tests/
-poetry run black --check creepycrawlai/ tests/
-poetry run flake8 creepycrawlai/ tests/ --darglint-ignore-regex '^test_.*'
-poetry run bandit -r --severity medium high creepycrawlai/ tests/
-poetry run vulture --min-confidence 100 creepycrawlai/ tests/
+poetry run mypy --ignore-missing-imports crawlai/ tests/
+poetry run isort --check --diff crawlai/ tests/
+poetry run black --check crawlai/ tests/
+poetry run flake8 crawlai/ tests/ --darglint-ignore-regex '^test_.*'
+poetry run bandit -r --severity medium high crawlai/ tests/
+poetry run vulture --min-confidence 100 crawlai/ tests/
 echo "Lint successful!"
